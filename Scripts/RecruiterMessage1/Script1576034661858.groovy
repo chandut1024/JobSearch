8e6d51e248ddfa9fd8413ea4c6427b63f7bd8a80
chandu.t <chandu.t@wlpt518.imidomain.com>
2019-12-11 11:04:51 +0530

    recruiter message

---
+++ Scripts/RecruiterMessage1/Script1576034661858.groovy
@@ -0,0 +1,49 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser(findTestData('LoginDetailsNJS').getValue(1, 1))
+
+WebUI.closeWindowIndex(1)
+
+WebUI.closeWindowIndex(1)
+
+WebUI.switchToWindowIndex(0)
+
+WebUI.maximizeWindow()
+
+WebUI.click(findTestObject('LoginButton'))
+
+WebUI.click(findTestObject('GoogleButton'))
+
+WebUI.switchToWindowIndex(1)
+
+WebUI.setText(findTestObject('UserName'), findTestData('LoginDetailsNJS').getValue(2, 1))
+
+WebUI.click(findTestObject('NextButton'))
+
+WebUI.delay(5)
+
+WebUI.setText(findTestObject('Password'), findTestData('LoginDetailsNJS').getValue(3, 1))
+
+WebUI.click(findTestObject('NextButton'))
+
+WebUI.switchToWindowIndex(0)
+
+WebUI.mouseOver(findTestObject('NotificationButton'))
+
+WebUI.delay(5)
+
+WebUI.click(findTestObject('RecruiterMessagesButton'))
+
